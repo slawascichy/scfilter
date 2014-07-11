d6478cb839d51f16a25907f969d78655914d8931
Sławomir Cichy <scichy@ibpm.pro>
2014-07-11 18:09:30 +0200

    Sprawy kosmetyczne (usunięcie niepotrzebnych komentarzy)

--- src/main/java/pl/slawas/filter/beans/SearchResult.java
+++ src/main/java/pl/slawas/filter/beans/SearchResult.java
@@ -60,122 +60,56 @@ public class SearchResult<Row extends ResultRow<Entity>, Entity> extends
 		return this.baseQueryResponse.getPageNr();
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see pl.wp.andro.paging._IPagingInfo#getAllPages()
-	 */
 	public Enumeration<Page> getAllPages() {
 		return this.baseQueryResponse.getAllPages();
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see pl.wp.andro.paging._IPagingInfo#getCurrentPageInfo()
-	 */
 	public Page getCurrentPageInfo() {
 		return this.baseQueryResponse.getCurrentPageInfo();
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see pl.wp.andro.paging._IPagingInfo#getFirstPageInfo()
-	 */
 	public Page getFirstPageInfo() {
 		return this.baseQueryResponse.getFirstPageInfo();
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * pl.wp.andro.paging._IPagingInfo#getFirstRowPositionOfPreviousResultRows()
-	 */
 	public Integer getFirstRowPositionOfPreviousResultRows() {
 		return this.baseQueryResponse.getFirstRowPositionOfPreviousResultRows();
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see pl.wp.andro.paging._IPagingInfo#getLastPageInfo()
-	 */
 	public Page getLastPageInfo() {
 		return this.baseQueryResponse.getLastPageInfo();
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see pl.wp.andro.paging._IPagingInfo#getNextPageInfo()
-	 */
 	public Page getNextPageInfo() throws PagedResultException {
 		return this.baseQueryResponse.getNextPageInfo();
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see pl.wp.andro.paging._IPagingInfo#getPageInfo(java.lang.Integer)
-	 */
 	public Page getPageInfo(Integer pageNumber) throws PagedResultException {
 		return this.baseQueryResponse.getPageInfo(pageNumber);
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see pl.wp.andro.paging._IPagingInfo#getPreviousPageInfo()
-	 */
 	public Page getPreviousPageInfo() throws PagedResultException {
 		return this.baseQueryResponse.getPreviousPageInfo();
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see pl.wp.andro.paging._IPagingInfo#hasPreviousResultRows()
-	 */
 	public boolean hasPreviousResultRows() {
 		return this.baseQueryResponse.hasPreviousResultRows();
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see pl.wp.andro.paging._IPagingInfo#getPagingParams()
-	 */
 	public PagingParams getPagingParams() {
 		return this.baseQueryResponse.getPagingParams();
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see pl.wp.andro.paging.ResultSupport#hasMoreResultRows()
-	 */
 	@Override
 	public boolean hasMoreResultRows() {
 		return this.baseQueryResponse.hasMoreResultRows();
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see pl.wp.andro.paging.ResultSupport#setHasMoreResultRows(boolean)
-	 */
 	@Override
 	public void setHasMoreResultRows(boolean hasMoreResultRows) {
 		throw new UnsupportedOperationException("Wynik jest tylko do odczytu");
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see pl.wp.andro.paging._IPagingInfo#getNumberOfRowsOnThePage()
-	 */
 	public int getNumberOfRowsOnThePage() {
 		return this.baseQueryResponse.getNumberOfRowsOnThePage();
 	}
